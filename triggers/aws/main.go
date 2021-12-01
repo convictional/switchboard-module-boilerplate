4d49557443b5d303bcf598e4b4bf05bc88043f43
keithconvictional <keith@convictional.com>
2021-12-01 10:22:07 -0500

    Got publish to SQS working

--- triggers/aws/main.go
+++ triggers/aws/main.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"log"
-	"switchboard-module-boilerplate/env"
+	"switchboard-module-boilerplate/load"
+	"switchboard-module-boilerplate/logging"
+	"switchboard-module-boilerplate/models"
 )
 
 func main() {
@@ -17,29 +16,20 @@ func main() {
 // HandleRequest is the entry point for AWS Lambda
 // https://docs.aws.amazon.com/lambda/latest/dg/golang-handler.html
 func HandleRequest(ctx context.Context, awsEvent AWSTriggerEvent) {
-	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{"stdout"}
-	if env.Debug() {
-		config.Level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
-	} else {
-		config.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
-	}
-	logger, err := config.Build()
-	if err != nil {
-		log.Fatalf("Failed to setup logger :: %+v", err)
-		return
-	}
+	logger := logging.GetLogger()
 
 	logger.Debug(fmt.Sprintf("AWS Events :: %+v", awsEvent))
 
-	// Convert event to be platform-agnostic
-	event, err := awsEvent.ConvertToTriggerEvent()
-	if err != nil {
-		logger.Error("Failed to convert trigger event", zap.Error(err))
-		return
-	}
+	load.PublishToSQS(models.Product{}, models.TriggerEvent{})
 
-	service := NewService(logger) // TODO - Move shared drive
-	service.Run(event)
+	// Convert event to be platform-agnostic
+	//event, err := awsEvent.ConvertToTriggerEvent()
+	//if err != nil {
+	//	logger.Error("Failed to convert trigger event", zap.Error(err))
+	//	return
+	//}
+	//
+	//service := NewService(logger) // TODO - Move shared drive
+	//service.Run(event)
 }
 
