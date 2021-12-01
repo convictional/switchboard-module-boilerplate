4990e7a418df538edf969e2873929719cb5476f8
Mitch Corish <mitch.corish@convictional.com>
2021-12-01 19:24:28 -0400

    updates to pubsub and HTTP triggers

--- triggers/gcp/httpEvent.go
+++ triggers/gcp/httpEvent.go
@@ -1,18 +1,18 @@
-package main
+
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"switchboard-module-boilerplate/env"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-// https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
-// triggerPubSub consumes a Pub/Sub message.
-func triggerPubSub(ctx context.Context, gcpPubSub GCPPubSubRecord) error {
+// httpTriggerEvent is an HTTP Cloud Function with a request parameter.
+func httpTriggerEvent(w http.ResponseWriter, r *http.Request) {
+
 	config := zap.NewProductionConfig()
 	config.OutputPaths = []string{"stdout"}
 	if env.Debug() {
@@ -23,20 +23,20 @@ func triggerPubSub(ctx context.Context, gcpPubSub GCPPubSubRecord) error {
 	logger, err := config.Build()
 	if err != nil {
 		log.Fatalf("Failed to setup logger :: %+v", err)
-		return err
+		return
 	}
 
-	logger.Debug(fmt.Sprintf("AWS Events :: %+v", gcpPubSub))
+	logger.Debug(fmt.Sprintf("AWS Events :: %+v", r))
 
 	// Convert event to be platform-agnostic
-	event, err := gcpPubSub.ConvertPSToTriggerEvent()
+	event, err := gcpPubSub.ConvertToTriggerEvent()
 	if err != nil {
 		logger.Error("Failed to convert trigger event", zap.Error(err))
-		return err
+		return
 	}
 
 	service := NewService(logger) // TODO - Move shared drive
 	service.Run(event)
 
-	return err
+	return
 }
--- triggers/gcp/models.go
+++ triggers/gcp/models.go
@@ -3,17 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"switchboard-module-boilerplate/models"
 )
 
-type GCPRecord struct {
+type GCPPubSubRecord struct {
 	MessageID   string `json:"messageId"`
 	Data        []byte `json:"data"`
 	Attributes  string `json:"attributes"`
 	OrderingKey string `json:"orderingKey"`
 }
 
-func (b *GCPRecord) ConvertToTriggerEvent() (models.TriggerEvent, error) {
+type GCPWebhookEvent struct {
+	Id              string         `json:"_id"`
+	Type            string         `json:"type"`
+	Created         string         `json:"created"`
+	CompanyObjectId string         `json:"companyObjectId`
+	Data            models.Product `json:"data"`
+}
+
+func (b *GCPPubSubRecord) ConvertPSToTriggerEvent() (models.TriggerEvent, error) {
 
 	var product models.Product
 	err := json.Unmarshal(b.Data, &product)
@@ -25,5 +35,23 @@ func (b *GCPRecord) ConvertToTriggerEvent() (models.TriggerEvent, error) {
 		ID:      b.MessageID,
 		Batch:   false,
 		Product: &product,
+	}, nil
+}
+
+func (r *http.Request) ConvertHTTPToTriggerEvent() (models.TriggerEvent, error) {
+	var body GCPWebhookEvent
+	body, err := io.ReadAll(r.Body)
+	//err := json.Unmarshal(io.ReadAll(r.Body), &body)
+
+	if err != nil {
+		return models.TriggerEvent{}, fmt.Errorf("failed to parse body into a webhook event :: %s", err.Error())
+	}
+
+	var product = &body.Data
+
+	return models.TriggerEvent{
+		ID:      body.Id,
+		Batch:   false,
+		Product: product,
 	}, nil
 }
