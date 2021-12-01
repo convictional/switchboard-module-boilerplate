50f9b562b59a9d45a31b39f2818c02a034661b3b
Keith Weaver <64023834+keithconvictional@users.noreply.github.com>
2021-12-01 08:58:05 -0500

    AWS Version (Runs with SQS test event) (#1)

--- extract/extract.go
+++ extract/extract.go
@@ -2,10 +2,10 @@ package extract
 
 import "switchboard-module-boilerplate/models"
 
-func Single(event models.TriggerEvent) models.Product {
-	return models.Product{}
+func Single(event models.TriggerEvent) (models.Product, error) {
+	return models.Product{}, nil
 }
 
-func Multiple(event models.TriggerEvent) []models.Product {
-	return []models.Product{}
+func Multiple(event models.TriggerEvent) ([]models.Product, error) {
+	return []models.Product{}, nil
 }
--- triggers/aws/main.go
+++ triggers/aws/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Printf("Test")
+	lambda.Start(HandleRequest)
 }
+
+// HandleRequest is the entry point for AWS Lambda
+// https://docs.aws.amazon.com/lambda/latest/dg/golang-handler.html
+func HandleRequest(ctx context.Context, awsEvent AWSTriggerEvent) {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	fmt.Printf("AWS Events :: %+v", awsEvent)
+	logger.Debug(fmt.Sprintf("AWS Events :: %+v", awsEvent))
+
+	// Convert event to be platform-agnostic
+	event, err := awsEvent.ConvertToTriggerEvent()
+	if err != nil {
+		logger.Error("Failed to convert trigger event", zap.Error(err))
+		return
+	}
+
+	service := NewService(logger) // TODO - Move shared drive
+	service.Run(event)
+}
+
